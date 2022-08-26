4a6296fadd7f6611c3e2bda71cd33dde721938c8
Junior Carvalho <antoniomarcosdecj18@gmail.com>
2022-08-26 19:40:53 -0300

    cumprindo o primeiro requisito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,86 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { saveUserEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    isButtonDisabled: true,
+  };
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value }, () => {
+      const MIN_PASSWORD = 5;
+      const { email, password } = this.state;
+      const emailVerify = (email.includes('@') && email.toLowerCase().includes('.com'));
+      const passwordVerify = (password.length > MIN_PASSWORD);
+      if (emailVerify && passwordVerify) {
+        this.setState({
+          isButtonDisabled: false,
+        });
+      } else {
+        this.setState({
+          isButtonDisabled: true,
+        });
+      }
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { email } = this.state;
+    const { dispatch, history } = this.props;
+
+    dispatch(saveUserEmail(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isButtonDisabled } = this.state;
+    return (
+      <div>
+        <form onSubmit={ this.handleSubmit }>
+          <label htmlFor="email">
+            Email:
+            <input
+              type="text"
+              id="email"
+              data-testid="email-input"
+              name="email"
+              value={ email }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input
+              type="password"
+              id="pasword"
+              data-testid="password-input"
+              name="password"
+              value={ password }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="submit"
+            disabled={ isButtonDisabled }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default Login;
+export default connect()(Login);
+
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,7 @@
 // Coloque aqui suas actions
+export const SAVE_USEREMAIL = 'SAVE_EMAIL';
+export const SAVE_USERWALLET = 'SAVE_USERWALLET';
+
+export const saveUserEmail = (email) => ({ type: SAVE_USEREMAIL, email });
+
+export const saveUserWallet = (userWallet) => ({ type: SAVE_USERWALLET, userWallet });
