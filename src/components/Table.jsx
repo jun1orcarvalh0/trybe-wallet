d38dec9b64495772cbafbf803542f1f6a2101f52
Junior Carvalho <antoniomarcosdecj18@gmail.com>
2022-08-28 15:48:19 -0300

    cumprindo o requisito 6

---
+++ src/components/Table.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+class Table extends Component {
+  render() {
+    return (
+      <div>
+        <table>
+          <thead>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </thead>
+          <tbody>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Table;
